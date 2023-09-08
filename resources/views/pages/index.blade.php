fcea9edfcf0f431c23f35747219363d9fd3f992b
Søren Risbøll Frisk <soeren.frisk+1@gmail.com>
2023-09-08 21:47:08 +0200

    first commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,15 @@
+<?php
+
+use function Livewire\Volt\{state};
+
+//
+
+?>
+
+<div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+    <div class="max-w-7xl mx-auto p-6 lg:p-8">
+        <div class="flex justify-center">
+            <img src="/dbench.svg" style="height:62px; width:62px;" />
+        </div>
+    </div>
+</div>
